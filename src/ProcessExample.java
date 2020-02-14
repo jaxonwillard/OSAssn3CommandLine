5d0164cc657655af4b295ddf7a456aad4d9897c3
Jaxon Willard <jaxonbwillard@gmail.com>
2020-02-13 19:56:21 -0700

    history and query history works

--- src/ProcessExample.java
+++ src/ProcessExample.java
@@ -16,33 +16,33 @@ public class ProcessExample {
         File fileDir = new File(currentDir);
         System.out.printf("The parent folder is: %s\n", fileDir.getParent());
 
-        // java.nio.file.Path proposed = java.nio.file.Paths.get(currentDir, "test");
-        // System.setProperty("user.dir", proposed.toString());
-        // System.out.printf("Updated Directory: %s\n", System.getProperty("user.dir"));
+         java.nio.file.Path proposed = java.nio.file.Paths.get(currentDir, "test");
+         System.setProperty("user.dir", proposed.toString());
+         System.out.printf("Updated Directory: %s\n", System.getProperty("user.dir"));
 
-        String[] command = {"git", "ProcessExample.java"};
-        ProcessBuilder pb = new ProcessBuilder(command);
-        pb.directory(new File(System.getProperty("user.dir")));
-        pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
-        pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+//        String[] command = {"git", "ProcessExample.java"};
+//        ProcessBuilder pb = new ProcessBuilder(command);
+//        pb.directory(new File(System.getProperty("user.dir")));
+//        pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
+//        pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
         
 
-        try {
-            long start = System.currentTimeMillis();
-            Process p = pb.start();
-
-
-            System.out.println("Starting to wait");
-            p.waitFor();
-            long end = System.currentTimeMillis();
-            System.out.printf("Waited for %d milliseconds\n", end - start);
-        }
-        catch (IOException ex) {
-            System.out.printf("Illegal command\n, %s\n", ex);
-        }
-        catch (Exception ex) {
-            System.out.println("Something else bad happened");
-        }
+//        try {
+//            long start = System.currentTimeMillis();
+//            Process p = pb.start();
+//
+//
+//            System.out.println("Starting to wait");
+//            p.waitFor();
+//            long end = System.currentTimeMillis();
+//            System.out.printf("Waited for %d milliseconds\n", end - start);
+//        }
+//        catch (IOException ex) {
+//            System.out.printf("Illegal command\n, %s\n", ex);
+//        }
+//        catch (Exception ex) {
+//            System.out.println("Something else bad happened");
+//        }
     }
 }
 
--- src/shell.java
+++ src/shell.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,32 +11,87 @@ public class shell {
         long totalExtProgramRunTime = 0;
         Scanner input = new Scanner(System.in);
         System.out.print(System.getProperty("user.dir") + "\n-$ ");
+        ArrayList<String[]> history = new ArrayList<>();
 
         while (input.hasNextLine()) {
             String line = input.nextLine();
-            String[] command = line.split(" ");
-            switch (command[0]) {
-                case "":
-                    break;
-                case "exit":
-                    System.exit(-1);
-                case "history":
-                    System.out.println("you typed history!");
-                    break;
-                case "cd":
-                    changeDirectory(command);
-                    break;
-                case "ptime":
-                    System.out.println(totalExtProgramRunTime);
-                    break;
-                default:
-                    long start = System.currentTimeMillis();
-                    System.out.println(runExternalCommand(command));
-                    totalExtProgramRunTime += System.currentTimeMillis() - start;
-                }
+            String[] command = splitCommand(line);
+            history.add(command);
+            totalExtProgramRunTime += processCommand(command, history, totalExtProgramRunTime);
+
             System.out.print("\n" + System.getProperty("user.dir") + "\n-$ ");
         }
         input.close();
+    }
+
+    static long processCommand(String[] command, ArrayList<String[]> history, long totalExtProgramRunTime) throws IOException {
+        long localRunTime = 0;
+        switch (command[0]) {
+            case "":
+                break;
+            case "exit":
+                System.exit(-1);
+            case "history":
+                history(history);
+                break;
+            case "^":
+                queryHistory(Integer.parseInt(command[1]), history, totalExtProgramRunTime);
+                break;
+            case "cd":
+                changeDirectory(command);
+                break;
+            case "list":
+                list();
+                break;
+            case "ptime":
+                System.out.println(totalExtProgramRunTime);
+                break;
+            default:
+                long start = System.currentTimeMillis();
+                System.out.println(runExternalCommand(command));
+                localRunTime += System.currentTimeMillis() - start;
+        }
+        return localRunTime;
+    }
+
+    static void history(ArrayList<String[]> history){
+        for (int i=1; i<history.size()+1; i++){
+            System.out.printf("%d: %s\n", i, formatArray(history.get(i-1)));
+        }
+    }
+
+    static String formatArray(String[] array){
+        StringBuilder sb = new StringBuilder();
+        for (String s : array){
+            sb.append(s).append(" ");
+        }
+        return sb.toString();
+    }
+
+    static void queryHistory(int queryIndex, ArrayList<String[]> history, long totalExtProgramRunTime) throws IOException {
+        if (queryIndex > history.size()+1) return;
+        processCommand(history.get(queryIndex-1), history, totalExtProgramRunTime);
+    }
+
+    static void list() {
+        File fileDir = new File(System.getProperty("user.dir"));
+        for (int i = 0; i< Objects.requireNonNull(fileDir.list()).length; i++){
+            System.out.println(processFile(new File(Objects.requireNonNull(fileDir.list())[i])));
+        }
+    }
+
+    static String processFile(File child){
+        StringBuilder tr = new StringBuilder();
+        String permissions = (child.isDirectory() ? "t" : "f" ) +
+                (child.canRead() ? "t" : "f" ) +
+                (child.canWrite() ? "t" : "f" ) +
+                (child.canExecute() ? "t" : "f" );
+        String length = String.format("%10d", child.length());
+        Date lastModified = new Date(child.lastModified());
+
+        tr.append(permissions).append(" ").append(length).append(" ").append(lastModified).append(" ").append(child);
+
+        return tr.toString();
     }
 
     static void changeDirectory(String[] command) throws IOException {
@@ -46,6 +101,16 @@ public class shell {
         System.out.printf("The parent folder is: %s\n", fileDir.getParent());
         System.out.println("fileDir.getAbsoluteFile() = " + fileDir.getAbsoluteFile());
         System.out.println("fileDir.createNewFile(\"test\") = " + fileDir.createNewFile());
+        System.out.println("fileDir.list() = " + Arrays.toString(fileDir.list()));
+
+
+        System.out.println("Arrays.toString(fileDir.list()) = " + Arrays.toString(fileDir.list()));
+        java.nio.file.Path proposed = java.nio.file.Paths.get(String.valueOf(fileDir), ".git");
+        File proposedFile = new File(String.valueOf(proposed));
+        System.out.println("proposedFile.isDirectory() = " + proposedFile.isDirectory());
+        System.out.println("proposedFile.isFile() = " + proposedFile.isFile());
+
+
     }
 
     static String runExternalCommand(String[] command) {
