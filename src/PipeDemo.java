cca911aeea2fba696c2990b128ded688001e9b3b
Jaxon Willard <jaxonbwillard@gmail.com>
2020-02-17 21:09:43 -0700

    moving to linux...

--- src/PipeDemo.java
+++ src/PipeDemo.java
@@ -1,28 +1,40 @@
-//
-//import java.io.IOException;
-//import java.util.*;
-//
-//public class ProcessBuilderExample
-//{
-//        public static void main(String[] args)
-//                throws IOException, InterruptedException
-//        {
-//                // you need a shell to execute a command pipeline
-//                List<String> commands = new ArrayList<String>();
-//                commands.add("/bin/sh");
-//                commands.add("-c");
-//                commands.add("ls -l /var/tmp | grep foo");
-//
-//                SystemCommandExecutor commandExecutor = new SystemCommandExecutor(commands);
-//                int result = commandExecutor.executeCommand();
-//
-//                StringBuilder stdout = commandExecutor.getStandardOutputFromCommand();
-//                StringBuilder stderr = commandExecutor.getStandardErrorFromCommand();
-//
-//                System.out.println("STDOUT");
-//                System.out.println(stdout);
-//
-//                System.out.println("STDERR");
-//                System.out.println(stderr);
-//        }
-//}
+import java.lang.*;
+
+public class PipeDemo {
+
+    public static void main(String[] args) {
+        String[] p1Cmd = { "cat", "shell.java"};
+        String[] p2Cmd = { "grep", ".canRead" };
+
+        ProcessBuilder pb1 = new ProcessBuilder(p1Cmd);
+        ProcessBuilder pb2 = new ProcessBuilder(p2Cmd);
+
+        pb1.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        //pb1.redirectOutput(ProcessBuilder.Redirect.PIPE);
+
+        pb2.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+
+        try {
+//            Process p1 = pb1.start();
+            Process p2 = pb2.start();
+
+//            java.io.InputStream in = p1.getInputStream();
+            java.io.OutputStream out = p2.getOutputStream();
+
+            int c;
+//            while ((c = in.read()) != -1) {
+//                out.write(c);
+//            }
+
+            out.flush();
+            out.close();
+
+//            p1.waitFor();
+            p2.waitFor();
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+}
